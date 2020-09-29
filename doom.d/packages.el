d869e35d42011d0877d3924d8dee6084b6293aa9
Johan Widén <j.e.widen@gmail.com>
2020-09-29 12:48:00 +0200

    Fix whitespace-style. projectile. Hydras for Helm and projectile

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -37,6 +37,7 @@
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
 (package! ace-link)
+(package! ag)
 (package! bibtex-completion)
 (package! bookmark+
   :recipe (:host github :repo "emacsmirror/bookmark-plus"))
